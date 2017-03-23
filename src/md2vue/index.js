41704f26d6551e76dcf8d346a69adfb0b1853cb3
gongbingyuan <gongbingyuan@houbank.cn>
2017-03-23 17:20:02 +0800

    optimization

--- src/md2vue/index.js
+++ src/md2vue/index.js
@@ -8,14 +8,21 @@ const STATIC_PATH = '../server/static/'
 const PAGE_PATH = resolve(join(__dirname, STATIC_PATH, 'pages'))
 
 export default function (mdPath, opts = {}) {
+    let exclude, watcher
+
     mdPath = resolve(mdPath)
+    exclude = [/node_modules/]
     
     if (!existsSync(mdPath)) {
         logger.error(`markdown file directory not exist`)
         return
     }
 
-    let watcher = chokidar.watch(mdPath, { ignored: opts.exclude })
+    if (opts.exclude) {
+        exclude = exclude.concat(opts.exclude)
+    }
+
+    watcher = chokidar.watch(mdPath, { ignored: exclude })
     watcher.on('add', (path) => correctExt(path) && add(path, getPagePath(path)))
     watcher.on('change', (path) => correctExt(path) && change(path, getPagePath(path)))
     watcher.on('unlink', (path) => correctExt(path) && remove(path, getPagePath(path)))
